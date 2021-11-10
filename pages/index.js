2c66e927b1d6acbf3cb0784ac128b5e6dd3e1a25
Eric Jinks <ericjinks@gmail.com>
2021-11-10 14:14:24 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Home({ time }) {
   return (
     <div className="container">
       <Head>
@@ -15,9 +15,17 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>{time}<p>
       </main>
 
       <Footer />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const time = new Date().toString()
+  return {
+    props: { time }, // will be passed to the page component as props
+  }
 }
